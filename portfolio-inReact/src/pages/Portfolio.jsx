70da23eeba8de4d0a596f66c834c46c2bc8b0c71
danielhe27 <jdah49d@gmail.com>
2024-05-13 16:26:30 -0400

    fixing path in images

--- portfolio-inReact/src/pages/Portfolio.jsx
+++ portfolio-inReact/src/pages/Portfolio.jsx
@@ -3,13 +3,56 @@ import React from 'react';
 const PortfolioPage = () => {
     // Array of projects with their details
     const projects = [
-        { id: 1, title: 'PWA-WebFlow', description: 'Progressive web application', image: '../src/assets/project-images/pwa.jpeg', link: 'https://github.com/danielhe27/PWA-WebFlow', deploy:'https://rocky-wildwood-58113-0dc7f9910c44.herokuapp.com' },
-        { id: 2, title: 'Blogspot-MVC', description: 'Model-View-Controller', image: '../src/assets/project-images/mvc.jpeg', link: 'https://github.com/danielhe27/blogspot-mvc', deploy:'https://secure-earth-02146-fc74d51ab940.herokuapp.com' },
-        { id: 3, title: 'note-storage', description: 'Create and store notes', image: '../src/assets/project-images/notecreator.jpeg', link: 'https://github.com/danielhe27/note-storage', deploy:'https://note-storage-8c599a9c6b47.herokuapp.com/notes' },
-        { id: 4, title: 'movie-decision-maker', description: 'Omdb Api helps with  choosing a movie  ', image: '../src/assets/project-images/movie-desicion.jpeg', link: 'https://github.com/danielhe27/movie-decision-maker/tree/main', deploy:'https://danielhe27.github.io/movie-decision-maker/' },
-        { id: 5, title: 'ChronoSaver', description: 'Time management web application', image: '../src/assets/project-images/chronosaver.jpeg', link: 'https://github.com/danielhe27/chronosaver', deploy:'https://danielhe27.github.io/chronosaver/'  },
-        { id: 6, title: 'web-based quiz application', description: 'trivia', image: '../src/assets/project-images/quiz.jpeg', link: 'https://github.com/danielhe27/SqueezyQuiz', deploy:'https://danielhe27.github.io/SqueezyQuiz/' },
+        {
+            id: 1,
+            title: 'PWA-WebFlow',
+            description: 'Progressive web application',
+            image: '/project-images/pwa.jpeg',
+            link: 'https://github.com/danielhe27/PWA-WebFlow',
+            deploy: 'https://rocky-wildwood-58113-0dc7f9910c44.herokuapp.com'
+        },
+        {
+            id: 2,
+            title: 'Blogspot-MVC',
+            description: 'Model-View-Controller',
+            image: '/project-images/mvc.jpeg',
+            link: 'https://github.com/danielhe27/blogspot-mvc',
+            deploy: 'https://secure-earth-02146-fc74d51ab940.herokuapp.com'
+        },
+        {
+            id: 3,
+            title: 'note-storage',
+            description: 'Create and store notes',
+            image: '/project-images/notecreator.jpeg',
+            link: 'https://github.com/danielhe27/note-storage',
+            deploy: 'https://note-storage-8c599a9c6b47.herokuapp.com/notes'
+        },
+        {
+            id: 4,
+            title: 'movie-decision-maker',
+            description: 'Omdb Api helps with choosing a movie',
+            image: '/project-images/movie-desicion.jpeg',
+            link: 'https://github.com/danielhe27/movie-decision-maker/tree/main',
+            deploy: 'https://danielhe27.github.io/movie-decision-maker/'
+        },
+        {
+            id: 5,
+            title: 'ChronoSaver',
+            description: 'Time management web application',
+            image: '/project-images/chronosaver.jpeg',
+            link: 'https://github.com/danielhe27/chronosaver',
+            deploy: 'https://danielhe27.github.io/chronosaver/'
+        },
+        {
+            id: 6,
+            title: 'web-based quiz application',
+            description: 'trivia',
+            image: '/project-images/quiz.jpeg',
+            link: 'https://github.com/danielhe27/SqueezyQuiz',
+            deploy: 'https://danielhe27.github.io/SqueezyQuiz/'
+        },
     ];
+    
 
     return (
         // Main container for the portfolio section
